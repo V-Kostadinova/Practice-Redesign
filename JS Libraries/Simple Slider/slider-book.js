dba1cf40d84b2469fcfa463f8bcc2526c06928ef
V-Kostadinova <37293901+V-Kostadinova@users.noreply.github.com>
2018-05-08 10:41:15 +0300

    Create slider-book.js

---
+++ JS Libraries/Simple Slider/slider-book.js
@@ -0,0 +1,21 @@
+(function(){
+  $('#carousel').carousel({ interval: 4000 });
+}());
+
+(function(){
+  $('.carousel-showmanymoveone .item').each(function(){
+    var itemToClone = $(this);
+
+    for (var i=1;i<4;i++) {
+      itemToClone = itemToClone.next();
+
+      if (!itemToClone.length) {
+        itemToClone = $(this).siblings(':first');
+      }
+
+      itemToClone.children(':first-child').clone()
+        .addClass("cloneditem-"+(i))
+        .appendTo($(this));
+    }
+  });
+}());
